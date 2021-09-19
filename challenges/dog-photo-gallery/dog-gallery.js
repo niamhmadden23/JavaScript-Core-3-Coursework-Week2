2f319f8eaede65df1b3e9c669f8b215d9f50215b
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2021-09-19 13:26:31 +0100

    dog gallery complete

---
+++ challenges/dog-photo-gallery/dog-gallery.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("button");
+const unorderedList = document.getElementById("list");
+const lists = document.createElement("li");
+unorderedList.appendChild(lists);
+const images = document.createElement("img");
+lists.appendChild(images);
+lists.style.listStyle = "none";
+
+button.addEventListener("click", getImages);
+
+function getImages() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+      //console.log(response);
+      return response.json();
+    })
+    .then((result) => {
+      //console.log(result);
+      images.src = result.message;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
